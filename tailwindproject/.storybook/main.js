6345f35b1aa727d1ba63a71d820f027c5383afe3
lakshay17 <lakshayjain01lj@gmail.com>
2025-02-01 21:35:22 +0530

    configure Storybook with Vite and add RevenueCard stories

--- tailwindproject/.storybook/main.js
+++ tailwindproject/.storybook/main.js
@@ -1,7 +1,23 @@
+import { mergeConfig } from 'vite';
+
 export default {
     stories: ['../src/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: ['@storybook/addon-essentials'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-styling-webpack'
+    ],
     core: {
-      builder: '@storybook/builder-vite', // 👈 The builder enabled here.
+        builder: '@storybook/builder-vite',
     },
-  };
+    async viteFinal(config) {
+        // Merge custom configuration into the default config
+
+        return mergeConfig(config, {
+            // Add dependencies to pre-optimization
+            optimizeDeps: {
+                include: ['storybook-dark-mode'],
+            },
+        });
+    },
+};
---
+++ tailwindproject/src/stories/components/RevenueCard.stories.jsx
@@ -0,0 +1,24 @@
+import { RevenueCard } from "../../components/RevenueCard";
+
+const meta = {
+    component: RevenueCard,
+};
+
+export default meta;
+
+
+export const BigAmount = {
+    args: {
+        orderCount: "123",
+        title: "Amount pending",
+        amount: "92,312.20",
+    },
+};
+
+export const SmallAmount = {
+    args: {
+        orderCount: "13",
+        title: "Amount pending",
+        amount: "921",
+    }
+}
